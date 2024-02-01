be11e3180dc18144b629211e68c2a5cbe8c6e468
janthony25 <junanthony.business@gmail.com>
2024-02-01 22:44:42 +1300

    Add project files.

---
+++ Exception/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // exception = errors that occur during execution
+
+            //          try    = try some code that is considered "dangerous"
+            //          catch  = catches and handles exceptions when they occur
+            //          finally = always executes regardless if exception is caught or not
+
+            double x;
+            double y;
+            double result;
+            try
+            {
+                Console.Write("Enter number 1: ");
+                x = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter number 2: ");
+                y = Convert.ToDouble(Console.ReadLine());
+
+                result = x / y;
+
+                Console.WriteLine($"result: {result}");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Please enter a valid number");
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Cannot divide by zero");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Something Went Wrong");
+            }
+            finally
+        {
+            Console.WriteLine("Thank you for using Divider");
+        }
+              
+           
+
+            Console.ReadKey();
+        }
+    }
